601b676f2c178c72ddc58ad16625b80fdbf8198b
Tittoh <tittohb@gmail.com>
2023-05-30 02:16:42 +0300

    Add support for multiple image urls.

--- main.py
+++ main.py
@@ -6,31 +6,41 @@ from PIL import Image
 app = Flask(__name__)
 
 @app.route('/process_image', methods=['POST'])
-def process_image():
+def process_images():
     data = request.get_json()
-    image_link = data.get('image_link')
-    unique_id = data.get('unique_id')
+    image_urls = data.get('image_urls')
 
-    try:
-        response = requests.get(image_link, stream=True)
-        response.raise_for_status()
-        image = Image.open(response.raw)
-        text = pytesseract.image_to_string(image)
+    results = []
+    for image_url in image_urls:
+        try:
+            response = requests.get(image_url, stream=True)
+            response.raise_for_status()
+            image = Image.open(response.raw)
+            text = pytesseract.image_to_string(image)
 
-        # Remove line breaks and replace with spaces
-        text = text.replace('\n', ' ')
+            # Remove line breaks and replace with spaces
+            text = text.replace('\n', ' ')
 
+            result = {
+                'url': image_url,
+                'text': text
+            }
+            results.append(result)
+        except Exception as e:
+            error_message = {
+                'url': image_url,
+                'error': str(e)
+            }
+            results.append(error_message)
 
-        result = {
-            'id': unique_id,
-            'text': text
-        }
-        return jsonify(result)
-    except Exception as e:
-        error_message = {
-            'error': str(e)
-        }
-        return jsonify(error_message), 400
+    return jsonify(results)
+
+@app.route('/<path:path>')
+def catch_all(path):
+    error_message = {
+        'error': 'Endpoint not found'
+    }
+    return jsonify(error_message), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
