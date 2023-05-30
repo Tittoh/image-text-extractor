192a5aa1a7950a949faeaf1ca9d6c6ef4f127c80
Tittoh <tittohb@gmail.com>
2023-05-30 04:11:09 +0300

    limit max urls to 8

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pytesseract
 from PIL import Image
 import os
 from werkzeug.exceptions import BadRequest
+import logging
 
 app = Flask(__name__)
 
@@ -11,8 +12,12 @@ def validate_request(data):
     if 'image_urls' not in data:
         raise BadRequest("Missing 'image_urls' field in the request.")
     image_urls = data['image_urls']
+
     if not isinstance(image_urls, list) or not all(isinstance(url, str) for url in image_urls):
         raise BadRequest("'image_urls' must be a list of strings.")
+
+    if len(image_urls) > 8:
+        raise BadRequest("Maximum number of URLs allowed is 8.")
 
 @app.route('/process_images', methods=['POST'])
 def process_images():
@@ -23,6 +28,8 @@ def process_images():
 
         results = []
         for image_url in image_urls:
+            # Extract the image ID from the URL
+            image_id = os.path.splitext(os.path.basename(image_url))[0]
             try:
                 response = requests.get(image_url, stream=True)
                 response.raise_for_status()
@@ -32,8 +39,6 @@ def process_images():
                 # Remove line breaks and replace with spaces
                 text = text.replace('\n', ' ')
 
-                # Extract the image ID from the URL
-                image_id = os.path.splitext(os.path.basename(image_url))[0]
 
                 result = {
                     'id': image_id,
@@ -51,6 +56,7 @@ def process_images():
     except BadRequest as e:
         return jsonify({'error': str(e)}), 400
     except Exception as e:
+        logging.exception("Exception occurred during image processing")
         return jsonify({'error': 'An internal server error occurred.'}), 500
 
 @app.route('/<path:path>')
